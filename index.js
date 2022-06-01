c956f741f31d6ca62e955ee40ffc5cdc90b66386
sakaitakumi <sakataku.0908@icloud.com>
2022-06-01 11:19:34 +0900

    j

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const piece01 = document.querySelector('.pos-1');
-const piece02 = document.querySelector('.pos-2');
-const piece03 = document.querySelector('.pos-3');
-const piece04 = document.querySelector('.pos-4');
-const piece05 = document.querySelector('.pos-5');
-const piece06 = document.querySelector('.pos-6');
-const piece07 = document.querySelector('.pos-7');
-const piece08 = document.querySelector('.pos-8');
-const piece09 = document.querySelector('.pos-9');
-const piece010 = document.querySelector('.pos-10');
+const piece0a = document.querySelector('.pos-a');
+for(a==1){
+    a=a+1
+}
 
 piece01.style.order = parseInt(Math.random() * 16) + 1;
