f0e818ca0b56f9f2108ef253afe42544e9f912d8
sakaitakumi <sakataku.0908@icloud.com>
2022-06-08 09:50:33 +0900

    j

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
-const piece01 = document.querySelector('.pos-1');
-const piece02 = document.querySelector('.pos-2');
-const piece03 = document.querySelector('.pos-3');
-const piece04 = document.querySelector('.pos-4');
-const piece05 = document.querySelector('.pos-5');
-const piece06 = document.querySelector('.pos-6');
-const piece07 = document.querySelector('.pos-7');
-const piece08 = document.querySelector('.pos-8');
-const piece09 = document.querySelector('.pos-9');
-const piece010 = document.querySelector('.pos-10');
-const piece011= document.querySelector('.pos-11');
-const piece012 = document.querySelector('.pos-12');
-const piece013 = document.querySelector('.pos-13');
-const piece014 = document.querySelector('.pos-14');
-const piece015 = document.querySelector('.pos-15');
-const piece016 = document.querySelector('.pos-16');
-
-piece01.style.order = parseInt(Math.random() * 16) + 1;
-piece02.style.order = parseInt(Math.random() * 16) + 1;
-piece03.style.order = parseInt(Math.random() * 16) + 1;
-piece04.style.order = parseInt(Math.random() * 16) + 1;
-piece05.style.order = parseInt(Math.random() * 16) + 1;
-piece06.style.order = parseInt(Math.random() * 16) + 1;
-piece07.style.order = parseInt(Math.random() * 16) + 1;
-piece08.style.order = parseInt(Math.random() * 16) + 1;
-piece09.style.order = parseInt(Math.random() * 16) + 1;
-piece010.style.order = parseInt(Math.random() * 16) + 1;
-piece011.style.order = parseInt(Math.random() * 16) + 1;
-piece012.style.order = parseInt(Math.random() * 16) + 1;
-piece013.style.order = parseInt(Math.random() * 16) + 1;
-piece014.style.order = parseInt(Math.random() * 16) + 1;
-piece015.style.order = parseInt(Math.random() * 16) + 1;
-piece016.style.order = parseInt(Math.random() * 16) + 1;
+for (let n = 1; n <= 16; n = n + 1) {
+    const piece = document.querySelector('.pos-' + n);
+  
+    // MEMO: 1 ～ 16 でランダムに数値を決めると、番号が被る場合がある
+    piece.style.order = parseInt(Math.random() * 16) + 1;
+  }
