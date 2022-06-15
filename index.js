c1e360f954668a0840bf0afa8d0aa1668dc9047f
sakaitakumi <sakataku.0908@icloud.com>
2022-06-15 09:57:40 +0900

    j

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ for (let n = 1; n <= 15; n = n + 1) {
 
    piece.addEventListener('click',pieceClickHandler);
 }  
+
+//State 相当の値を準備
+var peaces = [
+  6, 4, 3, 10,
+  7, 1, 2, 5,
+  9, 13, 12, 8,
+  15, 14, 16, 11, 
+];
+
+//Component 相当の関数の準備
+function Component(){
+  for (let n = 1; n <= 16; n = n + 1) {
+    const piece = document.querySelector('.pos-' + n);
+
+   piece.style.order = peaces[n];
+  }
+}  
