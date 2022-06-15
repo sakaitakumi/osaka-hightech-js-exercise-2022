2390cc8e69f02978ee88c1c03fb31be01d96c596
sakaitakumi <sakataku.0908@icloud.com>
2022-06-15 11:48:57 +0900

    j

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function pieceClickHandler(event) {
      console.log('下にいるので、移動して OK');
 
      const temp = peaces[15];
-     peaces[15] = peaces[N+1];
+     peaces[15] = peaces[N-4];
      peaces[N-1] = temp;
  
      component();
