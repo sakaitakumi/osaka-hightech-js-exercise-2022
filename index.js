b6125e48a303f494ba98d9bd6859c8fee448af0e
sakaitakumi <sakataku.0908@icloud.com>
2022-06-15 11:05:38 +0900

    j2

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+var up;    //空白ピース基準で1つ上のピースを記録
+var down;  //空白ピース基準で1つ下のピースを記録
+var left;  //空白ピース基準で1つ左のピースを記録
+var right; //空白ピース基準で1つ右のピースを記録
+
+var peaces = [
+  16, 4, 3, 10,
+  7, 1, 2, 5,
+  9, 13, 12, 8,
+  15, 14, 11, 6,
+];
+
+//空白ピースを基準に、上下左右の計算する処理を追加
+function calcAdjacentPeaces(){
+  const empty = peaces[15];
+
+  let temp_up = empty-4;
+  let temp_down = empty+4;
+  let temp_left = empty-1;
+  let temp_right = empty+1;
+
+  if(temp_up <1)temp_up = null;
+  if(temp_down>16)temp_down = null;
+  if(temp % 4===1)temp_left = null;
+  if(temp % 4===0)temp_right = null;
+
+  up = temp_up;
+  down = temp_down;
+  left = temp_left;
+  right = temp_right;
+}
+
 //State 相当の値を準備
 var peaces = [
   16, 4, 3, 10,
@@ -16,6 +48,7 @@ function Component(){
 }  
 
 Component();
+calcAdjacentPeaces();
 
 for (let n = 1; n <= 16; n = n + 1) {
     const piece = document.querySelector('.pos-' + n);
